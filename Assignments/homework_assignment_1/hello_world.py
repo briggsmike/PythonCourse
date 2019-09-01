771d88489be2743e07758fd35aade02d95c8f6a2
Michael Briggs <briggs@Michaels-MacBook-Pro.local>
2019-09-01 18:12:57 -0500

    1st run on Assignment 1

---
+++ Assignments/homework_assignment_1/hello_world.py
@@ -0,0 +1,34 @@
+#Michael Briggs
+#0456918
+#09/01/2019
+#MSITM 6341
+
+#For fun, I took a couple of takes on this one
+
+#1. Simple, one line. Not very clean in my opinion, but it works.
+print("Hello World!\nMy name is Michael Briggs\n\nThis is my first Python program!\n\nHave a nice day.")
+
+
+#2. Using variables, cleaner. Also trying out the title() method to correctly format a lowercase name
+    #first_name = "michael"
+    #last_name = "briggs"
+    #full_name = first_name + " " + last_name
+
+    #print("Hello World!" + "\nMy name is " + full_name.title() + "\n\nThis is my first Python program!" + "\n\nHave a nice day.")
+
+
+#3. Using more variables
+    #greeting = "Hello World!"
+    #intro = "My name is "
+    #name = "Michael Briggs"
+    #message = "This is my first Python program!"
+    #bye = "Have a nice day."
+
+    #One print statment
+    #print(greeting + "\n" + intro + name + "\n\n" + message + "\n\n" + bye)
+
+    #Or, broken out
+    #print(greeting)
+    #print (intro + name + "\n")
+    #print(message + "\n")
+    #print(bye)
