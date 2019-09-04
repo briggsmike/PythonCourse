0c67b27883803248d50d9fb3ff3159b3ccab363e
Michael Briggs <briggs@Michaels-MacBook-Pro.local>
2019-09-03 22:07:52 -0500

    Changed vars using int() and str() to tidy up

--- InClassWork/testing_input.py
+++ InClassWork/testing_input.py
@@ -5,18 +5,11 @@ print("\n\n\n")
 
 
 num1 = input("Enter a number ")
-num1 = int(num1)
 print(num1)
-
 num2 = input("Enter another number ")
-num2 = int(num2)
 print(num2)
 
-num3 = num1+num2
-
-num1 = str(num1)
-num2 = str(num2)
-num3 = str(num3)
+num3 = int(num1)+int(num2)
 
 print("\n")
-print(num1 + "+" + num2 + "=" + num3)
+print(num1 + "+" + num2 + "=" + str(num3))
