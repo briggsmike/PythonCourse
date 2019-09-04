e3563b0aebfd24056ea401035331e8584f81d067
Michael Briggs <briggs@Michaels-MacBook-Pro.local>
2019-09-03 21:43:20 -0500

    In class 9.3.19 testing input() and int to str

---
+++ InClassWork/testing_input.py
@@ -0,0 +1,22 @@
+message = input("What's your name: ")
+print("Hi " + message)
+
+print("\n\n\n")
+
+
+num1 = input("Enter a number ")
+num1 = int(num1)
+print(num1)
+
+num2 = input("Enter another number ")
+num2 = int(num2)
+print(num2)
+
+num3 = num1+num2
+
+num1 = str(num1)
+num2 = str(num2)
+num3 = str(num3)
+
+print("\n")
+print(num1 + "+" + num2 + "=" + num3)
